2cc2bf5927dcbf1a5d4a73d042f5929272259cbf
shadowkakarotbig-design <shadowkakarotbig@gmail.com>
2026-09-17 22:16:32 +0000

    Create commands/moderation/tagall.js

---
+++ commands/moderation/tagall.js
@@ -0,0 +1,27 @@
+module.exports = {
+  async execute(args, { sock, remoteJid }) {
+
+    if (!remoteJid.endsWith("@g.us")) {
+      return "❌ Cette commande fonctionne uniquement dans les groupes.";
+    }
+
+    const metadata = await sock.groupMetadata(remoteJid);
+    const participants = metadata.participants;
+
+    const mentions = participants.map(p => p.id);
+
+    let text = "🕊️ ANGEL NEVER CRY\n\n";
+    text += "📢 Mention de tous les membres\n\n";
+
+    for (const participant of participants) {
+      text += `@${participant.id.split("@")[0]} `;
+    }
+
+    await sock.sendMessage(remoteJid, {
+      text,
+      mentions
+    });
+
+    return null;
+  }
+};
