3841ebc7951bf686c81bc1a2f6f73870239e5f2f
shadowkakarotbig-design <shadowkakarotbig@gmail.com>
2026-09-17 15:45:56 +0000

    Create commands/general/menu.js

---
+++ commands/general/menu.js
@@ -0,0 +1,40 @@
+module.exports = {
+  name: "menu",
+  execute() {
+    return `
+╭━━━〔 🕊 ANGEL NEVER CRY 〕━━━╮
+│
+│ 🕊menu
+│
+│ 🎵 MUSIQUE
+│ 🕊play
+│ 🕊stop
+│ 🕊skip
+│
+│ 🎌 ANIME / MANGA / WEBTOON
+│ 🕊protagoniste
+│ 🕊antagoniste
+│ 🕊secondary
+│
+│ 🎭 FUN
+│ 🕊ship
+│ 🕊couple
+│ 🕊aura
+│ 🕊dad
+│ 🕊mom
+│
+│ 🖼️ MÉDIA
+│ 🕊img
+│ 🕊video
+│
+│ 🛡️ MODÉRATION
+│ 🕊mute
+│ 🕊unmute
+│ 🕊antilink
+│ 🕊antibot
+│ 🕊autoreact
+│
+╰━━━━━━━━━━━━━━━━━━━━╯
+`;
+  }
+};
