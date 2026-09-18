bc77ca145314fac6fcfa0418dd304de0ac16c16d
shadowkakarotbig-design <shadowkakarotbig@gmail.com>
2026-09-18 05:57:55 +0000

    Update antagoniste.js

--- commands/anime/antagoniste.js
+++ commands/anime/antagoniste.js
@@ -1,16 +1,16 @@
 module.exports = {
   name: "antagoniste",
-  async execute(sock, msg, args) {
+
+  async execute(args, { sock, message, remoteJid }) {
+
     if (!args.length) {
-      return sock.sendMessage(msg.key.remoteJid, {
-        text: "🕊 Utilisation : 🕊antagoniste [anime/manga/webtoon]"
-      });
+      return "🕊️ Utilisation : 🕊️antagoniste [anime/manga/webtoon]";
     }
 
     const œuvre = args.join(" ");
 
-    await sock.sendMessage(msg.key.remoteJid, {
-      text: `😈 Antagoniste de : ${œuvre}\n\n🔎 Recherche en préparation...`
-    });
+    return `☠️ Antagoniste de : ${œuvre}
+
+🔎 Recherche en préparation...`;
   }
 };
