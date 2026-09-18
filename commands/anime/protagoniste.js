e0ab14804cb5852df91592648ceba2fb082dfb31
shadowkakarotbig-design <shadowkakarotbig@gmail.com>
2026-09-18 05:55:53 +0000

    Update protagoniste.js

--- commands/anime/protagoniste.js
+++ commands/anime/protagoniste.js
@@ -1,16 +1,16 @@
 module.exports = {
   name: "protagoniste",
-  async execute(sock, msg, args) {
+
+  async execute(args, { sock, message, remoteJid }) {
+
     if (!args.length) {
-      return sock.sendMessage(msg.key.remoteJid, {
-        text: "🕊 Utilisation : 🕊protagoniste [anime/manga/webtoon]"
-      });
+      return "🕊️ Utilisation : 🕊️protagoniste [anime/manga/webtoon]";
     }
 
     const œuvre = args.join(" ");
 
-    await sock.sendMessage(msg.key.remoteJid, {
-      text: `🎭 Protagoniste de : ${œuvre}\n\n🔎 Recherche en préparation...`
-    });
+    return `🎭 Protagoniste de : ${œuvre}
+
+🔎 Recherche en préparation...`;
   }
 };
