b4f3838f854f4ff424c9a8e0ea9fa75a3a15073b
shadowkakarotbig-design <shadowkakarotbig@gmail.com>
2026-09-18 05:58:15 +0000

    Update secondary.js

--- commands/anime/secondary.js
+++ commands/anime/secondary.js
@@ -1,16 +1,16 @@
 module.exports = {
   name: "secondary",
-  async execute(sock, msg, args) {
+
+  async execute(args, { sock, message, remoteJid }) {
+
     if (!args.length) {
-      return sock.sendMessage(msg.key.remoteJid, {
-        text: "🕊 Utilisation : 🕊secondary [anime/manga/webtoon]"
-      });
+      return "🕊️ Utilisation : 🕊️secondary [anime/manga/webtoon]";
     }
 
     const œuvre = args.join(" ");
 
-    await sock.sendMessage(msg.key.remoteJid, {
-      text: `👥 Personnages secondaires de : ${œuvre}\n\n🔎 Recherche en préparation...`
-    });
+    return `⭐ Personnage secondaire de : ${œuvre}
+
+🔎 Recherche en préparation...`;
   }
 };
